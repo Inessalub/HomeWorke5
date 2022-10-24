113af6ab0e7453c24a0d099b3a144cc85df5ead8
unknown <inessalub@gmail.com>
2022-10-24 12:21:59 +0400

    blok2

---
+++ blok1/Program.cs
@@ -0,0 +1,212 @@
+﻿
+void FillArray(int[] array, int min, int max)   
+{
+    for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(min, max);
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i]);
+}
+
+int MinValue (int[] array)
+{
+    int min = array[0];
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] < min) min = array[i];
+
+    return min;
+}
+
+int MaxValue(int[] array)
+{
+    int max = array[0];
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] > max) max = array[i];
+
+    return max;
+}
+
+void SumArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+        sum += array[i];
+    Console.WriteLine(sum);
+}
+
+void MultiplicationArray(int[] array)
+{
+    int mult = 1;
+    for (int i = 0; i < array.Length; i++)
+        mult *= array[i];
+    Console.WriteLine(mulriplication);
+}
+
+int IndexElement(int[] array, int N)
+{
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] == N)
+        {
+            return i;
+        }
+    return -1;
+}
+
+bool ElementArray(int[] array, int value)
+{
+    bool x = false;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] == value) x = true;
+    if (x == true) Console.WriteLine("Нашли число в массиве");
+    else Console.WriteLine("Не нашли число в массиве");
+    return x;
+}
+
+void MeanArray(int[] array)
+{
+    double sum = 0;
+    for (int i = 0; i < array.Length; i++)
+        sum = (sum + array[i]);
+    Console.WriteLine(sum / array.Length);
+}
+
+int NegativeElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] < 0) count++;
+    return count;
+}
+
+int Count(int[] array, int a)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] == a) count++;
+    return count;
+}
+
+void ChetElement(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] % 2 == 0) count++;
+    Console.WriteLine(count);
+}
+
+int PositiveElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] > 0) count++;
+    return count;
+}
+
+void NechetElement(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] % 2 != 0) count++;
+    Console.WriteLine(count);
+}
+
+bool SortArray(int[] array)
+{
+    bool sort = true;
+    for (int i = 1; i < array.Length; i++)
+        if (array[i] < array[i - 1]) sort = false;
+    if (sort == false) Console.WriteLine();
+    else Console.WriteLine();
+    return sort;
+}
+
+Console.WriteLine("Размер массива:  ");
+int size = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine(); 
+int[] array = new int[size];
+
+
+FillArray(array, 0, 10);
+Console.WriteLine("Массив:   ");
+
+
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine();
+
+
+int minvalue = MinValue(array);
+Console.WriteLine("Минимальное значение: " + minvalue);
+Console.WriteLine();
+
+
+int maxvalue = MaxValue(array);
+Console.WriteLine("Максимальное значение: " + maxvalue);
+Console.WriteLine();
+
+
+Console.WriteLine("Сумма элементов массива:");
+SumArray(array);
+Console.WriteLine();
+
+
+Console.WriteLine("Произведение элементов массива:");
+MultiplicationArray(array);
+Console.WriteLine();
+
+
+Console.WriteLine("Введите искомый элемент  массива:  (ищем индекс)");
+int N = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine();
+int result = IndexElement(array, N);
+Console.WriteLine($"Заданный элемент массива соответсвует индексу {result}");
+Console.WriteLine();
+
+
+Console.WriteLine("Проверим, есть ли число в массиве? Введите число: ");
+int value = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine();
+bool elementArray = ElementArray(array, value);
+Console.WriteLine(elementArray);
+Console.WriteLine();
+
+
+Console.WriteLine("Среднее арифметическое элеметов массива:");
+MeanArray(array);
+Console.WriteLine();
+
+
+int NegElements = NegativeElements(array);
+Console.WriteLine("Кол-во отрицательных элементов в массиве: " + NegElements);
+Console.WriteLine();
+
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine();
+int count = Count(array, a);
+Console.WriteLine($"Заданное число встречается в массиве {count} раз.");
+Console.WriteLine();
+
+
+Console.WriteLine("Кол-во четных элементов в массиве:");
+ChetElement(array);
+Console.WriteLine();
+
+
+int PosElements = PositiveElements(array);
+Console.WriteLine("Кол-во положительных элементов в массиве: " + PosElements);
+Console.WriteLine();
+
+
+Console.WriteLine("Кол-во нечетных элементов в массиве:");
+NechetElement(array);
+Console.WriteLine();
+
+Console.WriteLine("является ли массив отсортированным по возрастанию? ");
+bool sortArray = SortArray(array);
+Console.WriteLine(sortArray);
+Console.WriteLine();
+
---
+++ blok2/Program.cs
@@ -0,0 +1,111 @@
+﻿// II. Реализовать следующие функции:
+// 1. Функцию, которая вычисляет число a в степени 
+
+int Stepen(int stp, int number)
+{
+    int count = 0;
+    int newnumber = 1;
+    for (int i = 0; i < stp; i++)
+        if (count < stp) newnumber *= number;
+    count++;
+    return newnumber;
+}
+Console.WriteLine("Введите число, которое необходимо возвести в степень: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("В какую степень возвести число?: ");
+int stp = int.Parse(Console.ReadLine() ?? "0");
+int stepen = Stepen(stp, number);
+Console.WriteLine($"Число {number} в степени {stp} равно {stepen}");
+
+// 2. Функцию, которая вычисляет факториал числа n
+
+void Factorial(int x)
+{
+    int factorial = 1;
+    for (int i = 1; i <= x; i++)
+        factorial *= i;
+    Console.WriteLine(factorial);
+}
+Console.WriteLine("Введите число искомого факториала: ");
+int x = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Факториал числа {x} равен ");
+Factorial(x);
+
+// 3. Функцию, которая вычисляет сумму цифр произвольного целого числа n
+
+void SumNumeral(int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+        if (n > 0)
+        {
+            sum += n % 10;
+            n /= 10;
+        }
+    Console.WriteLine(sum);
+}
+Random rand = new Random();
+int n = rand.Next(0, 1000);
+Console.WriteLine($"Сумма чисел в числе {n}");
+SumNumeral(n); 
+
+// 4. Функцию, которая проверяет является ли заданное число n полиндромом
+bool Polindrome(string number)
+{
+    for (int i = 0; i < number.Length / 2; i++)
+
+        if (number[i] != number[number.Length - i - 1])
+            return false;
+    return true;
+}
+Console.Write("Введите число и узнаем, является ли оно полиндромом ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+string num = number.ToString();
+bool polindrom = Polindrome(num);
+Console.WriteLine(polindrom);
+
+// 5. Функцию, складывающую два целых числа
+void SumNumbers(int a, int b)
+{
+    int number = 0;
+    if (a % 1 == 0 && b % 1 == 0) number = a + b;
+    Console.WriteLine(number);
+}
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Сумма чисел {a} и {b} равна: ");
+SumNumbers(a, b);
+
+// 6. Функцию, определяющую является ли число простым, то есть возвращающую true, если число простое, иначе - false
+
+bool Number(int n)
+{
+    for (int i = 2; i < n; i++)
+
+        if (n % i == 0) return false;
+
+    return true;
+}
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Число простое? ");
+bool num = Number(n);
+Console.WriteLine(num);
+
+// 7. Функцию, определяющую является ли число чётным, то есть возвращающую true, если число чётное, иначе - false
+
+bool ChetNumber(int number)
+{
+    bool n = false;
+    if (number % 2 == 0) n = true;
+    if (n == true) Console.WriteLine();
+    else Console.WriteLine();
+    return n;
+}
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Число четное? ");
+bool chetnumber = ChetNumber(number);
+Console.WriteLine(chetnumber);
